b649f62129b8df49e17693b2b1d9ebc224f6adac
WilcoApp <hello@wilco.gg>
2023-01-29 13:56:30 +0000

    Gitting Started

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,26 +1,18 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import styled from "styled-components";
-import theme from "../../styles/theme";
 
 const Banner = () => {
-  const BannerBackground = styled.div`
-    background-color: ${theme.dark};
-  `;
-
   return (
-    <BannerBackground>
-      <div className="banner text-white">
-        <div className="container p-4 text-center">
-          <img src={logo} alt="banner" />
-          <div>
-            <span>A place to </span>
-            <span id="get-part">get</span>
-            <span> the cool stuff.</span>
-          </div>
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span>A place to </span>
+          <span id="get-part">get</span>
+          <span> the cool stuff.</span>
         </div>
       </div>
-    </BannerBackground>
+    </div>
   );
 };
 
